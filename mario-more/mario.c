423c2bc3aae1fc46cd9acfe059d55612ea1450b0
ostuxcat <ameya@ostuxcat.in>
2026-02-05 19:08:45 +0530

    mario-more

---
+++ mario-more/mario.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void show_pyramid(int n);
+int main(){
+    int n;
+   do {
+    printf("%s", "Height: ");
+    scanf("%d", &n);
+    } while (n<1 || n>8);
+    show_pyramid(n);
+    return 0;
+    return 0;
+}
+void show_pyramid(int n){
+    int pattern = 1;
+    int spaces = n-pattern;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=spaces;j++){
+            printf("%s", " ");
+        }
+        for (int k=1;k<=pattern;k++){
+            printf("%s", "#");
+        }
+        printf("%s", "  ");
+        for (int k=1;k<=pattern;k++){
+            printf("%s", "#");
+        }
+        spaces-=1;
+        pattern+=1;
+        printf("\n");
+    }
+}
