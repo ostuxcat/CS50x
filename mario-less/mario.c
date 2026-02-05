501b778c14d0c319b31031df904dfa6b9dcd1540
ostuxcat <ameya@ostuxcat.in>
2026-02-05 19:37:46 +0530

    mario-less

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 void show_pyramid(int n);
 int main(){
-    int n;
+    int n=0;
    do {
     printf("%s", "Height: ");
     scanf("%d", &n);
-    } while (n<=0);
+    } while (n<1);
    show_pyramid(n);
     return 0;
 }
